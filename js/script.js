6fc94624b078e8e11e84b2618d0ebf41acbaf3aa
Stefano-Bernini <stefanobernini26@gmail.com>
2023-05-31 10:24:07 +0200

    aggiunta di commenti

--- js/script.js
+++ js/script.js
@@ -1,19 +1,25 @@
+// funzione per capire se la parola inserita sia palindroma
 function checkPalindroma(parola){
     let palindroma = parola.split('').reverse().join('');
+    // se palindroma restituisco true
     if(palindroma.toLowerCase() === parola.toLowerCase()){
         return true;
     }
-
+    // altrimenti restituisco false
     return false;
 }
 
+// input dell' utente
 let word = prompt('inserire una parola')
 
+// chiamo la funzione e l'assegno a check
 let check = checkPalindroma(word);
 
+// se e' true "stampo" che la parola e' palindroma
 if(check){
     console.log("La parola e' palindroma")
 }
+// se e' false "stampo" che la parola non e' palindroma
 else{
     console.log("La parola non e' palindroma")
 }
